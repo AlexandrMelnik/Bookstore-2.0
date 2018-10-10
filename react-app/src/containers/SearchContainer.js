6c1b78f55236da370382a0f11a345fb16c82ca2d
AlexMelnik <chanelsponky@gmail.com>
2018-10-10 21:20:48 +0300

    Add PropTypes

--- react-app/src/containers/SearchContainer.js
+++ react-app/src/containers/SearchContainer.js
@@ -1,9 +1,24 @@
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import SearchPage from '../components/SearchPage';
+
+SearchPage.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    categories_id: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequire,
+  loading: PropTypes.bool.isRequire
+}
 
 function mapStateToProps(state, ownProps) {
   return {
-    books: state.books.books.filter(book => book.title.includes(ownProps.match.params.query))
+    books: state.books.books.filter(book => book.title.includes(ownProps.match.params.query)),
+    loading: state.books.loading
   }
 }
 
