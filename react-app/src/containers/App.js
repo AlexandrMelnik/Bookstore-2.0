6d335b978ef65999809f561c196cef71cd04fc80
AlexMelnik <chanelsponky@gmail.com>
2018-10-10 21:05:19 +0300

    Add getBooksAll()

--- react-app/src/containers/App.js
+++ react-app/src/containers/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getAllBooks } from '../redux/actions/books';
 import PropTypes from 'prop-types';
 import Routes from '../Routes';
 import Header from './HeaderContainer';
 import Footer from '../components/Footer';
 
-const App = ({ location, search_books }) => (
-  <div className="app">
-    <Header />
-    <Routes location={ location } />
-    <Footer />
-  </div>
-);
+class App extends Component {
+  componentDidMount() {
+    this.props.getAllBooks();  
+  }
+
+  render() {
+    const { location, history } = this.props;
+    return (
+      <div className="app">
+        <Header history={history} />
+        <Routes location={ location } />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 App.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    categories_id: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+  getAllBooks: PropTypes.func.isRequired,
   location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   }).isRequired
@@ -21,8 +41,10 @@ App.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    books: state.books.books,
+    loading: state.books.loading,
     locale: state.locale
   }
 }
 
-export default connect(mapStateToProps, null)(App);
+export default connect(mapStateToProps, { getAllBooks })(App);
