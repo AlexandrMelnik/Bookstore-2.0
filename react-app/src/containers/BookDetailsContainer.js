93a9fcb41b67f37d9acc718f962d22aee94b0c91
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 15:57:46 +0300

    Add book to cart action

--- react-app/src/containers/BookDetailsContainer.js
+++ react-app/src/containers/BookDetailsContainer.js
@@ -1,18 +1,20 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getBookId } from '../redux/actions/books';
+import { addBookToCart } from '../redux/actions/cart';
 import BookDetailsPage from '../components/BookDetailsPage';
 
 BookDetailsPage.propTypes = {
   book: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  addBookToCart: PropTypes.func.isRequired
 }
 
-function mapStateToProps(state, props) {
+function mapStateToProps(state) {
   return {
     book: state.books.book,
     loading: state.books.loading,
   }
 }
 
-export default connect(mapStateToProps, { getBookId })(BookDetailsPage);
+export default connect(mapStateToProps, { getBookId, addBookToCart })(BookDetailsPage);
