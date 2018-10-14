d32a72f8e8e231fd87e711a5d5eb8d026b95b48e
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 23:28:36 +0300

    + Add localStorage.cart to reducer

--- react-app/src/redux/reducers/cart.js
+++ react-app/src/redux/reducers/cart.js
@@ -1,7 +1,8 @@
 import {
   ADD_BOOK_TO_CART,
   UPDATE_QUANTITY,
-  DELETE_CART_ITEM
+  DELETE_CART_ITEM,
+  UPDATE_BOOK_TO_CART
 } from '../actions/types';
 
 const initialState = {
@@ -14,22 +15,27 @@ export default function cart(state = initialState, action = {}) {
       const existIndex = findBookIndex(state.items, action.items.id);
       if(existIndex !== -1) {
         state.items[existIndex].count += 1;
+        localStorage.cart = JSON.stringify([...state.items]);
         return {
           items: [...state.items]
         };
+      } else {
+        localStorage.cart = JSON.stringify([...state.items, action.items]);
+        return {
+          items: [...state.items, action.items]
+        };
       }
-      return {
-        items: [...state.items, action.items]
-      };
 
     case UPDATE_QUANTITY:
       const existItemIndex = findBookIndex(state.items, action.items.id);
       if(state.items[existItemIndex].count === 1 && action.items.quantity === -1) {
+        localStorage.cart = JSON.stringify([...state.items]);
         return {
           items: [...state.items]
         };
       } else {
         state.items[existItemIndex].count += action.items.quantity;
+        localStorage.cart = JSON.stringify([...state.items]);
         return {
           items: [...state.items]
         };
@@ -37,9 +43,15 @@ export default function cart(state = initialState, action = {}) {
 
     case DELETE_CART_ITEM:
       const existItemDelete = findBookIndex(state.items, action.items.id);
+      localStorage.cart = JSON.stringify([...state.items.slice(0, existItemDelete), ...state.items.slice(existItemDelete+1)]);
       return {
         items: [...state.items.slice(0, existItemDelete), ...state.items.slice(existItemDelete+1)]
       }
+
+    case UPDATE_BOOK_TO_CART:
+        return {
+          items: [...action.items]
+        }
 
     default:
       return state;
