e0c8767590725775fdf6fc9785baac49c6f74fc3
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 22:05:10 +0300

    + Add method delete item from cart

--- react-app/src/redux/actions/cart.js
+++ react-app/src/redux/actions/cart.js
@@ -1,11 +1,20 @@
-import { ADD_BOOK_TO_CART, UPDATE_QUANTITY } from '../actions/types';
+import {
+  ADD_BOOK_TO_CART,
+  UPDATE_QUANTITY,
+  DELETE_CART_ITEM
+} from '../actions/types';
 
-export const addBookToCart = ({id, title, image, price, count = 1}) => ({
+export const addBookToCart = ({ id, title, image, price, count = 1 }) => ({
   type: ADD_BOOK_TO_CART,
   items: { id, title, image, price, count }
 });
 
-export const updateQuantityItem = ({id, quantity}) => ({
+export const updateQuantityItem = ({ id, quantity }) => ({
   type: UPDATE_QUANTITY,
   items: { id, quantity }
+});
+
+export const deleteCartItem = ({ id }) => ({
+  type: DELETE_CART_ITEM,
+  items: { id }
 });
