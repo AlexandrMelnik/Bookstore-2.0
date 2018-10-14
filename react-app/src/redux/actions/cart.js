fe4da5892e5874da0177b1d7521116039f6693cb
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 23:27:56 +0300

    + Add update cart

--- react-app/src/redux/actions/cart.js
+++ react-app/src/redux/actions/cart.js
@@ -1,12 +1,26 @@
 import {
   ADD_BOOK_TO_CART,
   UPDATE_QUANTITY,
-  DELETE_CART_ITEM
+  DELETE_CART_ITEM,
+  UPDATE_BOOK_TO_CART
 } from '../actions/types';
 
 export const addBookToCart = ({ id, title, image, price, count = 1 }) => ({
   type: ADD_BOOK_TO_CART,
   items: { id, title, image, price, count }
+});
+
+export const updateBookToCart = (cart) => ({
+  type: UPDATE_BOOK_TO_CART,
+  items: cart.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image: item.image,
+      price: item.price,
+      count: item.count
+    }
+  })
 });
 
 export const updateQuantityItem = ({ id, quantity }) => ({
