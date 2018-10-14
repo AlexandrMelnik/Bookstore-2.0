554f0e1cf597ac47341d481cdb28e58d6f09c026
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 15:58:22 +0300

    Add cart to type action

--- react-app/src/redux/actions/types.js
+++ react-app/src/redux/actions/types.js
@@ -1,23 +1,38 @@
+// Accounts
+// Register
 export const ACCOUNT_CREATE_START = 'ACCOUNT_CREATE_START';
 export const ACCOUNT_CREATE_SUCCESS = 'ACCOUNT_CREATE_SUCCESS';
 export const ACCOUNT_CREATE_FAILURE = 'ACCOUNT_CREATE_FAILURE';
 
+// Login
 export const ACCOUNT_LOGIN_START = 'ACCOUNT_LOGIN_START';
 export const ACCOUNT_LOGIN_SUCCESS = 'ACCOUNT_LOGIN_SUCCESS';
 export const ACCOUNT_LOGIN_FAILURE = 'ACCOUNT_LOGIN_FAILURE';
 
+// Lgout
 export const ACCOUNT_LOGOUT = 'ACCOUNT_LOGOUT';
 
+// Books
+// Fetch book from api
 export const FETCH_BOOKS_START = 'FETCH_BOOKS_START';
 export const FETCH_BOOKS_SUCCESS = 'FETCH_BOOKS_SUCCESS';
 export const FETCH_BOOKS_FAILURE = 'FETCH_BOOKS_FAILURE';
 
+// Categories
+// Fetch categoriest from api
 export const FETCH_CATEGORIES_SATART = 'FETCH_CATEGORIES_SATART';
 export const FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS';
 export const FETCH_CATEGORIES_FAILURE = 'FETCH_CATEGORIES_FAILURE';
 
+// Fetch book from id
 export const GET_BOOK = 'GET_BOOK';
 
+// Set localization
 export const SET_LOCALE = 'SET_LOCALE';
 
-export const SEARCH_BOOKS = 'SEARCH_BOOKS';
+// Cart
+// Add new item to cart
+export const ADD_BOOK_TO_CART = 'ADD_BOOK_TO_CART';
+
+// Update quantity item
+export const UPDATE_QUANTITY = 'UPDATE_QUANTITY';
