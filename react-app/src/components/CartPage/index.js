6c3e154266979f8e7b3e92ca2e235aab59e3fd1c
AlexMelnik <chanelsponky@gmail.com>
2018-10-14 16:00:51 +0300

    Create cart page

---
+++ react-app/src/components/CartPage/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import i18next from 'i18next';
+import { Panel, Col } from 'react-bootstrap';
+import CartItemsList from '../../containers/CartItemsListContainer';
+
+class CartPage extends Component {
+  render () {
+    const { itemsLength, cartItems } = this.props;
+    return (
+      <Col md={8} mdOffset={2}>
+        <Panel>
+          <Panel.Body>
+            <h4>
+               {i18next.t('items', {count: itemsLength})}
+            </h4>
+            <hr />
+
+            <CartItemsList  />
+            {!itemsLength
+              ? ''
+              : <h4>
+                 {i18next.t('total_price')}
+                 <span style={{color: 'green'}}>
+                   {' '}
+                   ${this.totalPrice(cartItems)}
+                 </span>
+               </h4>
+             }
+          </Panel.Body>
+        </Panel>
+      </Col>
+    );
+  }
+
+  totalPrice(cartItems) {
+    return cartItems.reduce((price, item) => {
+      price += item.price * item.count;
+      return price;
+    }, 0);
+  }
+}
+
+export default CartPage;
