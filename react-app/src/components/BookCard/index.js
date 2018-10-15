1c49d18ed9e75097f9e8105faa89c7e89eb26179
AlexMelnik <chanelsponky@gmail.com>
2018-10-15 23:42:55 +0300

    Fixed cart button

--- react-app/src/components/BookCard/index.js
+++ react-app/src/components/BookCard/index.js
@@ -9,7 +9,7 @@ const BookCard = (props) => (
   <Col xs={12} sm={6} md={4} lg={3}>
     <Panel>
       <Panel.Body className="thumbnail">
-        <Image src={props.image}  />
+        <Image src={props.image} />
         <h4>
           {props.title}
         </h4>
@@ -21,12 +21,15 @@ const BookCard = (props) => (
           starDimension="20px"
           starSpacing="2px"
         />
+        <h5 style={{color: 'green'}}>
+          ${props.price}
+        </h5>
         <hr />
         <Row className="show-grid">
           <Col xs={12} md={4}>
             <p><Button bsStyle="info" block onClick={() => props.addBookToCart(props)}><FontAwesome name='cart-plus' /></Button></p>
           </Col>
-          <Col  md={8}>
+          <Col  xs={12} md={8}>
             <Link to={'/book/' + props.id} style={{textDecoration: 'none'}}>
               <Button bsStyle="default" block>{i18next.t('details')}</Button>
             </Link>
