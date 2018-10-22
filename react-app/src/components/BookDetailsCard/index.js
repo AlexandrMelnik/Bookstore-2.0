881fab80f1f0fceb4c37183c33ad58a936d41153
AlexMelnik <chanelsponky@gmail.com>
2018-10-23 00:24:30 +0300

    + Added so that the button turns green when added to cart

--- react-app/src/components/BookDetailsCard/index.js
+++ react-app/src/components/BookDetailsCard/index.js
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import i18next from 'i18next';
 import FontAwesome from 'react-fontawesome';
 import StarRatings from 'react-star-ratings';
 import { Panel, Col, Row, Image, Button } from 'react-bootstrap';
 
-const BookDetailsCard = (props) => (
-  <Panel>
-    <Panel.Body>
-      <Row>
-        <Col xs={12} md={4} className="thumbnail">
-          <Image src={props.image}/>
-        </Col>
-        <Col xs={12} md={8}>
-          <h3>{props.title}</h3>
-          <StarRatings
-            rating={props.rating}
-            starDimension="20px"
-            starSpacing="2px"
-          />
-          <h4 style={{color: 'green'}}>
-            ${props.price}
-          </h4>
-          <p style={{marginTop: '1.300rem'}}>{props.desc}</p>
-          <Button bsStyle="info" onClick={() => props.addBookToCart(props)}>
-            <FontAwesome name='cart-plus' /> {i18next.t('add_to_cart')}
-          </Button>
-          <p style={{marginTop: '1.300rem'}}> {i18next.t('menu_categories')}: Drama, Fentesy </p>
-        </Col>
-      </Row>
-    </Panel.Body>
-  </Panel>
-);
+class BookDetailsCard extends Component {
+  state = {
+    addCartActiveClass: 'info',
+    addCartActiveIcon: 'shopping-cart'
+  }
+
+  addToCart (data) {
+    this.props.addBookToCart(data);
+    this.setState({
+      addCartActiveClass: 'success',
+      addCartActiveIcon: 'cart-plus' });
+  }
+
+  render() {
+    const {
+      image,
+      title,
+      price,
+      rating,
+      desc
+    } = this.props;
+
+    return (
+      <Panel>
+        <Panel.Body>
+          <Row>
+            <Col xs={12} md={4} className="thumbnail">
+              <Image src={image}/>
+            </Col>
+            <Col xs={12} md={8}>
+              <h3>{title}</h3>
+              <StarRatings
+                rating={rating}
+                starDimension="20px"
+                starSpacing="2px"
+              />
+              <h4 style={{color: 'green'}}>
+                $ {price}
+              </h4>
+              <p style={{marginTop: '1.300rem'}}>{ desc }</p>
+              <Button bsStyle={this.state.addCartActiveClass} onClick={this.addToCart.bind(this, this.props)}>
+                <FontAwesome name={this.state.addCartActiveIcon} /> {i18next.t('add_to_cart')}
+              </Button>
+              <p style={{marginTop: '1.300rem'}}> {i18next.t('menu_categories')}: Drama, Fentesy </p>
+            </Col>
+          </Row>
+        </Panel.Body>
+      </Panel>
+    );
+  }
+}
 
 export default BookDetailsCard;
