b5d6d531e1d3517a512aef667e58269b57ebba21
AlexMelnik <chanelsponky@gmail.com>
2018-10-24 21:55:41 +0300

    Replaced the class with a const, and fixed the loading

--- react-app/src/components/SearchPage/index.js
+++ react-app/src/components/SearchPage/index.js
@@ -3,21 +3,20 @@ import i18next from 'i18next';
 import { Label } from 'react-bootstrap';
 import BooksList from '../BooksList';
 
-class SearchPage extends Component {
-  render() {
-    const { books, loading } = this.props;
-    return (
-      <div className="search-page">
-        <h4>
-          <Label bsStyle="primary">
-             {i18next.t('search')}: {this.props.match.params.query} - {i18next.t('results')}: {books.length}
-          </Label>
-        </h4>
-        <hr />
-        {!loading ?  i18next.t('loading') : <BooksList books={books} />}
-      </div>
-    );
-  }
-}
+
+const SearchPage = ({ books, loading }) => (
+  <div className="search-page">
+    <h4>
+      <Label bsStyle="primary">
+         {i18next.t('search')}: {this.props.match.params.query} - {i18next.t('results')}: {books.length}
+      </Label>
+    </h4>
+    <hr />
+    {loading
+      ? <BooksList books={books} />
+      : i18next.t('loading')
+    }
+  </div>
+);
 
 export default SearchPage;
