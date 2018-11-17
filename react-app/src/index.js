2ad5d529daa4a0285c51a035f15643f1b6a6521d
AlexandrMelnik <39063311+AlexandrMelnik@users.noreply.github.com>
2018-11-17 17:50:32 +0200

    Update index.js
    Add comments

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -10,9 +10,12 @@ import decode from 'jwt-decode';
 import App from './containers/App';
 import './styles/modern.css';
 
+/* Create store settings */
 const store = configureStore();
 
+/* Сheck if there is a user token */
 if(localStorage.token) {
+  /* If there is, then we decode and log in */
   const token = decode(localStorage.token);
   const user = {
     token: localStorage.token,
@@ -21,13 +24,17 @@ if(localStorage.token) {
   store.dispatch(userLogin(user));
 }
 
+/* If there are goods in the local storage then added to the cart */
 if(localStorage.cart) {
   store.dispatch(updateBookToCart(JSON.parse(localStorage.cart)));
 }
 
+/* Language setting */
 if(localStorage.lang) {
+  /* If in local storage have a language and set it up */
   store.dispatch(setLocale(localStorage.lang));
 } else {
+  /* Otherwise, set the default language {en} */
   store.dispatch(setLocale('en'));
 }
 
