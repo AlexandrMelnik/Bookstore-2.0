426a9cc91930d3c3d96e8b27f1cfc87d5150665c
AlexandrMelnik <39063311+AlexandrMelnik@users.noreply.github.com>
2018-11-17 17:04:22 +0200

    Update SigninForm.js
    Added comments

--- react-app/src/components/Forms/SigninForm.js
+++ react-app/src/components/Forms/SigninForm.js
@@ -21,30 +21,58 @@ class SigninForm extends Component {
     errors: {}
   };
 
+  // If there was an error from the server then add to the state
   componentWillReceiveProps(nextProps) {
     this.setState({ errors: nextProps.serverErrors });
   }
 
+  /**
+   * Changing data from form
+   * @param {object} e 
+   */
   onChange = e => {
     this.setState({
       data: { ...this.state.data, [e.target.name]: e.target.value }
     });
   };
 
+  /**
+   * Submits the form
+   * @param {object} e 
+   */
   onSubmit = e => {
+    // Cancels page redirect
     e.preventDefault();
+
+    // We check validation
     const errors = this.validate(this.state.data);
     this.setState({ errors });
+
+    // If there are no errors, send data to the server
     if(Object.keys( errors ).length === 0) {
       this.setState({ loading: true });
+
+      /**
+       * Call the action from redux to send data to the server
+       * and pass data from the state
+       */
       this.props.signin(this.state.data);
       this.setState({ loading: false });
     }
   };
 
+  /**
+   * Checks validation
+   * @param {object} data
+   * @returns {object} errors
+   */
   validate = data => {
     const errors = {};
+
+    // Checks email validation
     if(!Validator.isEmail(data.email)) errors.email = i18next.t('message_email');
+
+    // Checks password validation
     if(!data.password) errors.password = i18next.t('message_password');
     return errors;
   };
